83d7767a10df870c22561341960301643647b8ff
Mateus Pozzi <mateuscarvalho88@gmail.com>
2016-09-14 23:10:41 -0300

    Adicionado Campo Numero Cartão e Mascara

---
+++ ValidadorCartaoCredito/ValidadorCartaoCredito/Models/DadosCartao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ValidadorCartaoCredito.Models
+{
+    public class DadosCartao
+    {
+        [MaxLength(19, ErrorMessage = "Máximo de caracteres excedido")]
+        [MinLength(19, ErrorMessage = "Mínimo de 19 caracteres")]
+        [DisplayName("Número do Cartão")]
+        [Required(ErrorMessage = "Preencher campo Numero do Cartão")]
+        public String NumeroCartao { get; set; }
+    }
+}
